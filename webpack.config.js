83d8f8b9f24514d1da6831f6634467759f81b9b3
krasimir-hr <125263294+krasimir-hr@users.noreply.github.com>
2025-04-19 18:02:01 +0300

    Quick fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
       clean: true,
-      publicPath: '/',
+      publicPath: '/weather-app/',
    },
    mode: 'development',
    module: {
